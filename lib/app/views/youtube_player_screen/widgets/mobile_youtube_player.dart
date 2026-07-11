a02c8abb861b8d64940837cd41b3fa764d5b4a70
TulasiReddy <tulasi.kagithala1@motorolasolutions.com>
2026-07-11 19:07:28 +0530

    Added Android permissions and refactored MobileYouTubePlayer to use YoutubePlayerScaffold

--- lib/app/views/youtube_player_screen/widgets/mobile_youtube_player.dart
+++ lib/app/views/youtube_player_screen/widgets/mobile_youtube_player.dart
@@ -11,51 +11,62 @@ class MobileYouTubePlayer extends StatefulWidget {
 }
 
 class _MobileYouTubePlayerState extends State<MobileYouTubePlayer> {
-  final YoutubePlayerController _youTubeController = YoutubePlayerController(
-    params: const YoutubePlayerParams(
-      showControls: true,
-      showFullscreenButton: true,
-      enableCaption: false,
-    ),
-  );
+  late YoutubePlayerController _youTubeController;
 
   @override
   void initState() {
     super.initState();
-    _youTubeController.loadVideoById(videoId: widget.videoId);
+    _youTubeController = YoutubePlayerController.fromVideoId(
+      videoId: widget.videoId,
+      autoPlay: true,
+      params: const YoutubePlayerParams(
+        showControls: true,
+        showFullscreenButton: true,
+        enableCaption: false,
+        mute: false,
+        showVideoAnnotations: false,
+        loop: false,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _youTubeController.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     final screenOrientation = MediaQuery.of(context).orientation;
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      backgroundColor: PURE_BLACK,
-      appBar: screenOrientation == Orientation.portrait ? AppBar(
-        backgroundColor: tealishBlue,
-        leading: IconButton(
-          onPressed: () => Navigator.of(context).pop(),
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.white,
+    return YoutubePlayerScaffold(
+      controller: _youTubeController,
+      aspectRatio: 16 / 9,
+      builder: (context, player) {
+        return Scaffold(
+          backgroundColor: PURE_BLACK,
+          appBar: screenOrientation == Orientation.portrait
+              ? AppBar(
+                  backgroundColor: tealishBlue,
+                  leading: IconButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                  ),
+                  title: const Text(
+                    "YouTube player",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                )
+              : null,
+          body: Center(
+            child: player,
           ),
-        ),
-        title: const Text(
-          "YouTube player",
-          style: TextStyle(fontSize: 18, color: Colors.white),
-        ),
-      ) : null,
-      body: Center(
-        child: SizedBox(
-          width: double.infinity,
-          child: YoutubePlayer(
-            controller: _youTubeController,
-            // aspectRatio: screenWidth / screenHeight,
-          ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
